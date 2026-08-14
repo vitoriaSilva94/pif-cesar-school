be77a27342291a4bfb703b740875a65b2c3b791f
vitoriaSilva94 <vsrs@cesar.school>
2026-08-14 19:13:29 +0000

    Exercícios 1, 2 e 3 adicionados.

---
+++ LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main () {
+    float numero = 3.1415;
+    printf("O seu número constante é de %.3f.\n",numero);
+
+    return 0;
+}
---
+++ LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main () {
+    int idade = 31;
+    printf("A sua idade é de %d anos.\n",idade);
+
+    return 0;
+}
