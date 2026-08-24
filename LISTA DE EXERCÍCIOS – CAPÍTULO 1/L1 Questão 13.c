0cfb72fce50a54271ae5a250a8c8a29432729e86
vitoriaSilva94 <vsrs@cesar.school>
2026-08-24 18:39:25 -0300

    Update L1 Questão 13.c

--- LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 13.c
+++ LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 13.c
@@ -1,5 +1,5 @@
-/*No desenvolvimento de programas em C, o que são conceitualmente os arquivos de
+No desenvolvimento de programas em C, o que são conceitualmente os arquivos de
 inclusão (headers com extensão .h)?
 a) São bibliotecas pré-compiladas em formato binário contendo funções estruturadas.
-*/
+
 
