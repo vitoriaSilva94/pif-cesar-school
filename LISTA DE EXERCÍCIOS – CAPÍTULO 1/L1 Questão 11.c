2c55c955abf07e0a41302079cce7651ba7b32fbd
vitoriaSilva94 <vsrs@cesar.school>
2026-08-24 18:36:51 -0300

    Update L1 Questão 11.c

--- LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 11.c
+++ LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 11.c
@@ -2,3 +2,20 @@
 correta (por exemplo: constante inteira decimal, constante de ponto flutuante, constante de caractere,
 constante string ou sequência de escape) e o tipo de dado base correspondente em C (como char, int,
 float, double):*/
+
+Constante Classificação (Tipo de Constante) Tipo Base em C
+\r [ Preencher ] [ Preencher ]
+2130 [ Preencher ] [ Preencher ]
+-123 [ Preencher ] [ Preencher ]
+33.28 [ Preencher ] [ Preencher ]
+0XFA [ Preencher ] [ Preencher ]
+0101 [ Preencher ] [ Preencher ]
+2.0e30 [ Preencher ] [ Preencher ]
+\xDC [ Preencher ] [ Preencher ]
+'\"' [ Preencher ] [ Preencher ]
+'\\' [ Preencher ] [ Preencher ]
+'F' [ Preencher ] [ Preencher ]
+0 [ Preencher ] [ Preencher ]
+'\0' [ Preencher ] [ Preencher ]
+"F" [ Preencher ] [ Preencher ]
+-4567.89 [ Preencher ] [ Preencher ]
