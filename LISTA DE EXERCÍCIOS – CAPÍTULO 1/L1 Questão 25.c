43b664ea9e176fa135fb5c7deb452de5c1cbdae2
vitoriaSilva94 <vsrs@cesar.school>
2026-08-24 17:46:59 +0000

    questao 25

--- LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 25.c
+++ LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 25.c
@@ -1,7 +1,6 @@
-/*Escreva um programa em C contendo uma única instrução printf() que desenhe a letra C
-em formato ampliado, utilizando a própria letra para a sua composição, conforme o modelo abaixo:
-
-CCCCC
-C
-C
-CCCCC*/
+#include <stdio.h>
+int main ()
+{
+    printf("CCCCC\nC\nC\nCCCCC");
+    return 0;
+}
