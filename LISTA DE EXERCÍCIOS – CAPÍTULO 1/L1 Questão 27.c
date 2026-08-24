29fd5926db9960c48a9d0414789a6a351f1b8242
vitoriaSilva94 <vsrs@cesar.school>
2026-08-24 17:42:25 +0000

    questão 27

--- LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 27.c
+++ LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 27.c
@@ -1,4 +1,19 @@
-/*Questão 27. Escreva um programa em C que solicite ao usuário (usando a função scanf()) um valor
-inteiro correspondente a um intervalo de tempo em segundos. O programa deve processar esse dado,
-calcular e exibir o equivalente formatado em Horas, Minutos e Segundos restantes (Exemplo: 3665
-segundos correspondem a 1 hora, 1 minuto e 5 segundos).*/
+
+#include <stdio.h>
+int main (){
+
+    int numero;
+
+
+    printf("Digite a quantidade de segundos:\n");
+    scanf("%d",&numero);
+
+    int minutos = numero/60;
+    int horas = minutos/60;
+    int restoMinutos = minutos%60;
+    int restoSegundos = numero%60;
+
+    printf("%d segundos é igual a %d horas, %d minutos e %d segundos.\n",numero ,horas ,restoMinutos ,restoSegundos);
+
+    return 0;    
+}
