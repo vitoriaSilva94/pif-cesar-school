4ef6661dc3e88a3c9a133b9639b5d81531db6974
vitoriaSilva94 <vsrs@cesar.school>
2026-08-25 12:23:38 +0000

    27, 28

--- LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 28.c
+++ LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 28.c
@@ -1,7 +1,4 @@
-/*Desenvolva um programa em C que leia três valores numéricos inteiros fornecidos pelo
-usuário através do teclado, calcule a média aritmética simples desses valores como um número real de
-dupla precisão (double) e exiba o resultado final na tela formatado com exatamente duas casas
-decimais.*/
+
 #include <stdio.h>
 int main ()
 {
