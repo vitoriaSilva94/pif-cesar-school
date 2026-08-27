60080496e0f53c83f8ea6ced4b00ab42f563180f
vitoriaSilva94 <vsrs@cesar.school>
2026-08-27 02:01:05 +0000

    atualização de nomes de arquivos

--- LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 21.c
+++ LISTA DE EXERCÍCIOS – CAPÍTULO 1/L1 Questão 21.c
@@ -1,7 +1,25 @@
-/*21. Desenvolva três versões independentes de programas em C para produzir no console a
-saída de texto abaixo. A primeira versão deve usar uma única chamada de printf(); a segunda deve usar
-exatamente duas instruções de impressão independentes; e a terceira deve desenhar as frases
-emolduradas utilizando caracteres gráficos de caixa:
 
-Treinamento em programação.
-Linguagem C.*/
+#include <stdio.h>
+int main ()
+{
+    printf("Treinamento em programação.\nLiguangem C.");
+
+    return 0;
+}
+
+#include <stdio.h>
+int main ()
+{
+    printf("Treinamento em programação.\n");
+    printf("Linguagem C.\n");
+
+    return 0;
+}
+
+#include <stdio.h>
+int main ()
+{
+     printf("\xC9 \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xBB \n\xBA Treinamento em programação. \xBA\n\xBA Linguagem C.                \xBA\n\xC8 \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xCD \xBC ");
+
+    return 0;
+}
